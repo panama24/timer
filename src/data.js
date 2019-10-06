d813bfd63ea614ab8a8544adf43b0e857b4e0f0f
Jessica <jessica@hiclark.com>
2019-10-05 20:00:43 -0400

    dummy data

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const timeline = {
+  class_start_offset: 5,
+  class_workout_offset: 29,
+  class_end_offset: 33,
+  segments: [
+    {
+      id: "1kj38dwkj38",
+      length: 6,
+      name: "cycling"
+    },
+    {
+      id: "90sdfg8sdfh",
+      length: 8,
+      name: "pushups"
+    },
+    {
+      id: "32lknr9wkn",
+      length: 5,
+      name: "squats"
+    },
+    {
+      id: "l2k48sdfnk",
+      length: 5,
+      name: "situps"
+    }
+  ]
+};
+
+export {
+  timeline
+};
